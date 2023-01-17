f572b8b93878f0bfdcc256b609f7ad40a6bfe904
Pratham Jain <prathamjainyt@gmail.com>
2023-01-18 00:51:00 +0530

    16/01/2023

--- cryptotracker/src/App.js
+++ cryptotracker/src/App.js
@@ -1,12 +1,16 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import './App.css';
 import Header from "./components/Header";
+import CoinPage from "./pages/CoinPage";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Header/>
+        <Route path="/" component={Homepage} />
+        <Route path="/coins/:id" component={CoinPage} />
       </div>
     </BrowserRouter>
   );
