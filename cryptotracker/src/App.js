3c2e97a471de769fb703f0e6813342d4eed0e162
AdityaGrover07 <adityagrover7oo@gmail.com>
2023-01-18 00:24:51 +0530

    16/01/2023

--- cryptotracker/src/App.js
+++ cryptotracker/src/App.js
@@ -1,11 +1,14 @@
-
+import { BrowserRouter } from "react-router-dom"
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header/>
+      </div>
+    </BrowserRouter>
   );
 }
 
