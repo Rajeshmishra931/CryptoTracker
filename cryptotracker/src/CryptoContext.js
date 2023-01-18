2ee3fb35ce72c11b210ebc0360c815c506bc1f87
Pratham Jain <prathamjainyt@gmail.com>
2023-01-18 23:18:57 +0530

    17/01/2023

--- cryptotracker/src/CryptoContext.js
+++ cryptotracker/src/CryptoContext.js
@@ -2,10 +2,24 @@ import React, { Children, createContext } from 'react'
 
 const Crypto = createContext()
 
-const CryptoContext = ({Children}) => {
+const CryptoContext = ({ children }) => {
+  const [currency, setCurrency] = useState("INR");
+  const [symbol, setSymbol] = useState("₹");
+
+  useEffect(() => {
+    if (currency === "INR") setSymbol("₹");
+    else if (currency === "USD") setSymbol("$");
+  }, [currency]);
+
   return (
-    <Crypto.Provider>{Children}</Crypto.Provider>
-  )
-}
+    <Crypto.Provider value={{ currency, setCurrency, symbol }}>
+      {children}
+    </Crypto.Provider>
+  );
+};
 
 export default CryptoContext;
+
+export const CryptoState = () => {
+  return useContext(Crypto);
+};
--- cryptotracker/src/components/Header.js
+++ cryptotracker/src/components/Header.js
@@ -29,6 +29,8 @@ const Header = () => {
 
   const history = useHistory();
 
+  const { currency, setCurrency } = CryptoState();
+
   const darkTheme =createTheme({
     palette: {
       primary: {
@@ -50,7 +52,10 @@ const Header = () => {
             width: 100,
             height: 40,
             marginRight: 15,
-          }}>
+          }}
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+          >
             <MenuItem value= {'USD'}>USD</MenuItem>
             <MenuItem value= {'INR'}>INR</MenuItem>
           </Select>
