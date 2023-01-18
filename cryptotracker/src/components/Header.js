7b005e292e2f1005099f38d759d46e3ed0f474c6
Rajesh Mishra <68376241+Rajeshmishra931@users.noreply.github.com>
2023-01-18 21:55:51 +0530

    17/01/2023

--- cryptotracker/src/components/Header.js
+++ cryptotracker/src/components/Header.js
@@ -1,8 +1,40 @@
+import { 
+  AppBar, 
+  Container,
+  makestyles, 
+  MenuItem, Select,
+  Toolbar,
+  Typography, 
+} from '@material-ui/core'
+
 import React from 'react'
+
+const useStyles = makeStyles(() => ({}
+
+))
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <AppBar color = 'transparent' position = 'static'>
+      <Container>
+        <Toolbar>
+          <Typography>
+            Crypto Hunter
+          </Typography>
+          <Select variant = 'outlined' 
+          style={{
+            width: 100,
+            height: 40,
+            marginleft: 15,
+          }}>
+            <MenuItem value= {'USD'}>USD</MenuItem>
+            <MenuItem value= {'INR'}>INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+
+    </AppBar>
   )
 }
 
