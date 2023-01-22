aa1a6031e9b2aee8377a18aa5718dde88981dea8
chetanya <chetanya78400@gmail.com>
2023-01-22 20:54:01 +0530

    22/01/2023

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -28,12 +28,64 @@ const CoinInfo = ({ coin }) => {
     },
   });
 
-  const useStyles = makeStyles(() => ({}));
+  const useStyles = makeStyles(() => ({
+
+    container: {
+      width: "75%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 25,
+      padding: 40,
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+        marginTop: 0,
+        padding: 20,
+        paddingTop: 0,
+      },
+    },
+  }));
   const classes = useStyles();
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <div className={classes.container}></div>
+      <div className={classes.container}>
+      
+      {!historicData | flag===false ? (
+          <CircularProgress
+            style={{ color: "gold" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+
+                datasets: [
+                  {
+                    data: historicData.map((coin) => coin[1]),
+                    label: `Price ( Past ${days} Days ) in ${currency}`,
+                    borderColor: "#EEBC1D",
+                  },
+                ],
+              }}
+              
+            />
+            
+              
+          </>
+        )}
+      </div>
     </ThemeProvider>
   );
 };
